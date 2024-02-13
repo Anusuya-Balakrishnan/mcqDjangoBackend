e4b917d0627747430d6ea38fdd3786fc55fe7e4f
Anusuya-Balakrishnan <anusuyabalakrishnan01@gmail.com>
2024-02-13 14:32:41 +0530

    cors header changes 2

--- mcqProject/settings.py
+++ mcqProject/settings.py
@@ -82,7 +82,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://mcqbackend-dzcukp3b2-anusuyas-projects-9666991e.vercel.app"
+    "https://mcqbackend-dzcukp3b2-anusuyas-projects-9666991e.vercel.app",
+    "https://mcqbackend.vercel.app/mcq/"
     # Add other origins as needed
 ]
 ROOT_URLCONF = 'mcqProject.urls'
